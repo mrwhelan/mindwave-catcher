de89bbd6db2c05885ad9a15c6ffcb6a6dcd172d9
Mark Whelan <m@townunderground.net>
2014-12-08 21:15:26 -0500

    apply function f to captured data

--- src/mindwave_catcher/core.clj
+++ src/mindwave_catcher/core.clj
@@ -5,24 +5,33 @@
   (:import  [java.io BufferedReader]
             [java.net Socket]))
 
-  (def startup (str "{\"enableRawOutput\": true, \"format\": \"Json\"}"))
+(defn startup-str
+  "Builds the startup command string. If enabled is set to true,
+   we will get all the EEG data sent, including readings from the
+   forehead sensor."
+  [enabled]
+  (str "{\"enableRawOutput\":"
+       enabled
+       ", \"format\": \"Json\"}"))
 
-  (defn- clojurize
-    "Turns json output into clojure data"
-    [data-line]
-    (-> data-line
-        (json/read-str)
-        (walk/keywordize-keys)))
+(defn- clojurize
+  "Turns json output into clojure data"
+  [data-line]
+  (-> data-line
+      (json/read-str)
+      (walk/keywordize-keys)))
 
-  (defn catch-waves
-    "Connects to the ThinkGear Connector at the host and port,
-     then prints the EEG data that issues forth, as Clojure maps."
-    [host port]
-    (with-open [sock (Socket. host port)
-                writer (io/writer sock)
-                reader (BufferedReader. (io/reader sock))]
-      (.append writer startup)
-      (.flush writer)
-      (doseq [line (line-seq reader)]
-        (let [clj-line (clojurize line)]
-          (if (:eSense clj-line) (println (:eegPower clj-line)))))))
+(defn catch-waves
+  "Connects to the ThinkGear Connector at the host and port,
+   causing EEG data to be captured. If raw-output-enabled is
+   set to true, all data will be sent including raw EEG data.
+   Data is converted to clojure maps as it arrives, and function
+   f will be applied to each map as it arrives."
+  [host port raw-output-enabled f]
+  (with-open [sock (Socket. host port)
+              writer (io/writer sock)
+              reader (BufferedReader. (io/reader sock))]
+    (.append writer (startup-str raw-output-enabled))
+    (.flush writer)
+    (doseq [line (line-seq reader)]
+      (f (clojurize line)))))
